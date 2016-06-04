73bdbc4b43679b8abd5da583181b247f32f429e9
Lütfi Tekin <lutfi-tekin@outlook.com>
2016-06-04 20:16:18 +0300

    added shouts fragment

--- app/src/main/java/com/incitorrent/radyo/menemen/pro/fragments/haykir.java
+++ app/src/main/java/com/incitorrent/radyo/menemen/pro/fragments/haykir.java
@@ -2,18 +2,23 @@ package com.incitorrent.radyo.menemen.pro.fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.incitorrent.radyo.menemen.pro.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class haykir extends Fragment {
-
+public class haykir extends Fragment implements View.OnClickListener {
+    EditText editText;
+    FloatingActionButton send;
+    RecyclerView shoutRV;
 
     public haykir() {
         // Required empty public constructor
@@ -23,8 +28,16 @@ public class haykir extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_haykir, container, false);
+        View haykirview = inflater.inflate(R.layout.fragment_haykir, container, false);
+        editText = (EditText) haykirview.findViewById(R.id.ETmesaj);
+        send = (FloatingActionButton) haykirview.findViewById(R.id.mesaj_gonder_button);
+        shoutRV = (RecyclerView) haykirview.findViewById(R.id.shoutRV);
+        send.setOnClickListener(this);
+        return haykirview;
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
