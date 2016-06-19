3f6c345036f74bc38f60d34871d67813007f1eac
Lütfi Tekin <lutfi-tekin@outlook.com>
2016-06-19 21:34:59 +0300

    added broadcast receiver for MUSIC_INFO_SERVICE

--- app/src/main/java/com/incitorrent/radyo/menemen/pro/services/MUSIC_INFO_SERVICE.java
+++ app/src/main/java/com/incitorrent/radyo/menemen/pro/services/MUSIC_INFO_SERVICE.java
@@ -68,7 +68,7 @@ public class MUSIC_INFO_SERVICE extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, final int startId) {
         Log.v(TAG, "ID: " +startId);
-        return START_NOT_STICKY;
+        return START_STICKY;
     }
 
     @Override
---
+++ app/src/main/java/com/incitorrent/radyo/menemen/pro/utils/MusicInfoReceiver.java
@@ -0,0 +1,22 @@
+package com.incitorrent.radyo.menemen.pro.utils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.incitorrent.radyo.menemen.pro.services.MUSIC_INFO_SERVICE;
+
+public class MusicInfoReceiver extends BroadcastReceiver {
+    //internet bağlantısı değiştiğinde veya telefon başlatıldığında MUSIC_INFO_SERVICE i başlat
+
+    public MusicInfoReceiver() {
+    }
+    Menemen m;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        m = new Menemen(context);
+     if(m.isInternetAvailable()) context.startService(new Intent(context, MUSIC_INFO_SERVICE.class));
+        Log.v("MusicInfoReceiver","onReceive isInternetAvailable" + m.isInternetAvailable());
+    }
+}
