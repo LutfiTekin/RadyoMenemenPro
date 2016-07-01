8d4651397b89c151598f418e7e58449862121bc1
Lütfi Tekin <lutfi-tekin@outlook.com>
2016-07-01 18:35:46 +0300

    removed radio pause feature when disconnected

--- app/src/main/java/com/incitorrent/radyo/menemen/pro/utils/MusicInfoReceiver.java
+++ app/src/main/java/com/incitorrent/radyo/menemen/pro/utils/MusicInfoReceiver.java
@@ -3,11 +3,8 @@ package com.incitorrent.radyo.menemen.pro.utils;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.preference.PreferenceManager;
-import android.util.Log;
 
 import com.incitorrent.radyo.menemen.pro.services.MUSIC_INFO_SERVICE;
-import com.incitorrent.radyo.menemen.pro.services.MUSIC_PLAY_SERVICE;
 
 public class MusicInfoReceiver extends BroadcastReceiver {
     private static final String TAG = "MusicInfoReceiver" ;
@@ -19,13 +16,14 @@ public class MusicInfoReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         m = new Menemen(context);
-        final Boolean notify_when_onair = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("notifications_on_air", true);
-     if(m.isInternetAvailable() && notify_when_onair) context.startService(new Intent(context, MUSIC_INFO_SERVICE.class));
-     if(!m.isInternetAvailable() && m.oku("caliyor").equals("evet")) {
-         //internet bağlantısı kopunca duraklat
-         context.startService(new Intent(context, MUSIC_PLAY_SERVICE.class));
-         Log.v(TAG,"Radyo Çalarken bağlantı gitti");
-     }
-        Log.v(TAG,"onReceive isInternetAvailable" + m.isInternetAvailable());
+//        final Boolean notify_when_onair = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("notifications_on_air", true);
+//     if(m.isInternetAvailable() && notify_when_onair) context.startService(new Intent(context, MUSIC_INFO_SERVICE.class));
+//     if(!m.isInternetAvailable() && m.oku("caliyor").equals("evet")) {
+//         //internet bağlantısı kopunca duraklat
+//         context.startService(new Intent(context, MUSIC_PLAY_SERVICE.class));
+//         Log.v(TAG,"Radyo Çalarken bağlantı gitti");
+//     }
+//        Log.v(TAG,"onReceive isInternetAvailable" + m.isInternetAvailable());
+        if(m.isInternetAvailable()) context.startService(new Intent(context, MUSIC_INFO_SERVICE.class));
     }
 }
