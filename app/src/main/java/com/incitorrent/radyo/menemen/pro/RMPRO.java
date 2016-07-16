f51584887cc5b4804fd2fab8a54546d9a13031d0
Lütfi Tekin <lutfi-tekin@outlook.com>
2016-07-16 19:15:24 +0300

    added shout topic

--- app/src/main/java/com/incitorrent/radyo/menemen/pro/RMPRO.java
+++ app/src/main/java/com/incitorrent/radyo/menemen/pro/RMPRO.java
@@ -38,6 +38,7 @@ public class RMPRO extends Application {
         FirebaseMessaging.getInstance().subscribeToTopic("general");
         FirebaseMessaging.getInstance().subscribeToTopic("news");
         FirebaseMessaging.getInstance().subscribeToTopic("sync");
+        FirebaseMessaging.getInstance().subscribeToTopic("shout");
     }
 
     public static Context getContext(){
