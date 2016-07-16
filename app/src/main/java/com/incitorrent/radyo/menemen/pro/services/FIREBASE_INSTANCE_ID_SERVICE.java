3ebe3dc0519732532f2c4ef8ace4fd96986555b3
Lütfi Tekin <lutfi-tekin@outlook.com>
2016-07-16 18:49:41 +0300

    added FIREBASE_INSTANCE_ID_SERVICE

---
+++ app/src/main/java/com/incitorrent/radyo/menemen/pro/services/FIREBASE_INSTANCE_ID_SERVICE.java
@@ -0,0 +1,16 @@
+package com.incitorrent.radyo.menemen.pro.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class FIREBASE_INSTANCE_ID_SERVICE extends Service {
+    public FIREBASE_INSTANCE_ID_SERVICE() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
