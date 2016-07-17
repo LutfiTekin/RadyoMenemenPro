23103d2b98ca359fc37807892cf90b919660dc46
Lütfi Tekin <lutfi-tekin@outlook.com>
2016-07-17 10:51:31 +0300

    added show image activity

---
+++ app/src/main/java/com/incitorrent/radyo/menemen/pro/show_image.java
@@ -0,0 +1,13 @@
+package com.incitorrent.radyo.menemen.pro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class show_image extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_image);
+    }
+}
