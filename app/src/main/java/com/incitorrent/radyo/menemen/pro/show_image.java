88176f92f7ff6dbe3f864cb0415f03241be6ff95
Lütfi Tekin <lutfi-tekin@outlook.com>
2016-07-17 11:04:13 +0300

    changed padding

--- app/src/main/java/com/incitorrent/radyo/menemen/pro/show_image.java
+++ app/src/main/java/com/incitorrent/radyo/menemen/pro/show_image.java
@@ -1,7 +1,10 @@
 package com.incitorrent.radyo.menemen.pro;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class show_image extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class show_image extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_image);
+
+        final Intent mintent = getIntent();
+        final Bundle myBundle=mintent.getExtras();
+        final Uri myURI=mintent.getData();
+        final String value;
+        if (myURI != null) {
+            Toast.makeText(show_image.this, myURI.toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
