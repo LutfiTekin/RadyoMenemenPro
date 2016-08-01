9fac1c4d4ea164d28b1bbd9a63b783d0d27866c7
Lütfi Tekin <lutfi-tekin@outlook.com>
2016-08-01 13:09:39 +0300

    change share and save buttons with overflow menu buttons

--- app/src/main/java/com/incitorrent/radyo/menemen/pro/show_image.java
+++ app/src/main/java/com/incitorrent/radyo/menemen/pro/show_image.java
@@ -3,16 +3,17 @@ package com.incitorrent.radyo.menemen.pro;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.incitorrent.radyo.menemen.pro.utils.TouchImageView;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -20,21 +21,16 @@ import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
 public class show_image extends AppCompatActivity implements View.OnClickListener {
-    public Button save,share;
-    private ImageView image;
+    private TouchImageView image;
     private String imageurl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_image);
-        image = (ImageView) findViewById(R.id.image);
-        save = (Button) findViewById(R.id.save);
-        share = (Button) findViewById(R.id.share);
+        image = (TouchImageView) findViewById(R.id.image);
         final Intent mintent = getIntent();
         final Uri imageUri=mintent.getData();
         imageurl = imageUri.toString().trim();
-        share.setOnClickListener(this);
-        save.setOnClickListener(this);
 
     }
 
@@ -56,61 +52,87 @@ public class show_image extends AppCompatActivity implements View.OnClickListene
 
     @Override
     public void onClick(View view) {
-        if(view == save){
-            final String root = Environment.getExternalStorageDirectory().toString();
-            final File compressed = new File(root + "/RadyoMenemen/images/compressed");
-            final String fname =  System.currentTimeMillis() + ".jpg";
-            final int width = image.getWidth();
-            final int height = image.getHeight();
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        File file = new File(compressed, fname);
-                        file.createNewFile();
-                        FileOutputStream out = new FileOutputStream(file);
-                        Bitmap bit = Glide.with(show_image.this).load(imageurl).asBitmap().into(width,height).get();
-                        bit.compress(Bitmap.CompressFormat.JPEG, 100, out);
-                        out.flush();
-                        out.close();
-                    } catch (InterruptedException | ExecutionException | IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
-            Toast.makeText(show_image.this, compressed.toString() + fname, Toast.LENGTH_SHORT).show();
-        }else if(view == share){
-            final String root = Environment.getExternalStorageDirectory().toString();
-            final File compressed = new File(root + "/RadyoMenemen/images/compressed");
-            final String fname =  System.currentTimeMillis() + ".jpg";
-            final int width = image.getWidth();
-            final int height = image.getHeight();
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        File file = new File(compressed, fname);
-                        file.createNewFile();
-                        FileOutputStream out = new FileOutputStream(file);
-                        Bitmap bit = Glide.with(show_image.this).load(imageurl).asBitmap().into(width,height).get();
-                        bit.compress(Bitmap.CompressFormat.JPEG, 100, out);
-                        out.flush();
-                        out.close();
-                    } catch (InterruptedException | ExecutionException | IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
+     //TODO handle fab click
+    }
 
-            String pic = compressed + "/" + fname;
-            Uri pictureUri = Uri.parse(pic);
-            Log.v("URI", "BUILD "+ pictureUri);
-            Intent shareIntent = new Intent();
-            shareIntent.setAction(Intent.ACTION_SEND);
-            shareIntent.putExtra(Intent.EXTRA_STREAM, pictureUri);
-            shareIntent.setType("image/*");
-            shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            startActivity(Intent.createChooser(shareIntent, getString(R.string.share)));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.image_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_save:
+            save();
+                break;
+            case R.id.action_share:
+            share();
+                break;
         }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void share() {
+        final String root = Environment.getExternalStorageDirectory().toString();
+        final File compressed = new File(root + "/RadyoMenemen/images/compressed");
+        final String fname =  System.currentTimeMillis() + ".jpg";
+        final int width = image.getWidth();
+        final int height = image.getHeight();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    File file = new File(compressed, fname);
+                    file.createNewFile();
+                    FileOutputStream out = new FileOutputStream(file);
+                    Bitmap bit = Glide.with(show_image.this).load(imageurl).asBitmap().into(width,height).get();
+                    bit.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                    out.flush();
+                    out.close();
+                } catch (InterruptedException | ExecutionException | IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        String pic = compressed + "/" + fname;
+        Uri pictureUri = Uri.parse(pic);
+        Log.v("URI", "BUILD "+ pictureUri);
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, pictureUri);
+        shareIntent.setType("image/*");
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(Intent.createChooser(shareIntent, getString(R.string.share)));
+    }
+
+    private void save() {
+        final String root = Environment.getExternalStorageDirectory().toString();
+        final File compressed = new File(root + "/RadyoMenemen/images/compressed");
+        final String fname =  System.currentTimeMillis() + ".jpg";
+        final int width = image.getWidth();
+        final int height = image.getHeight();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    File file = new File(compressed, fname);
+                    file.createNewFile();
+                    FileOutputStream out = new FileOutputStream(file);
+                    Bitmap bit = Glide.with(show_image.this).load(imageurl).asBitmap().into(width,height).get();
+                    bit.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                    out.flush();
+                    out.close();
+                } catch (InterruptedException | ExecutionException | IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        Toast.makeText(show_image.this, compressed.toString() + fname, Toast.LENGTH_SHORT).show();
     }
 }
