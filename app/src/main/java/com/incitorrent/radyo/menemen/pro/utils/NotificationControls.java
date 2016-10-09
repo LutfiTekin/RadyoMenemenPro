38c9ecaf056af9552754b3a4e145887807f2d43f
Lütfi Tekin <lutfi-tekin@outlook.com>
2016-10-09 16:31:02 +0300

    fix podcast bug, show loading toast

--- app/src/main/java/com/incitorrent/radyo/menemen/pro/utils/NotificationControls.java
+++ app/src/main/java/com/incitorrent/radyo/menemen/pro/utils/NotificationControls.java
@@ -3,7 +3,10 @@ package com.incitorrent.radyo.menemen.pro.utils;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
+import com.incitorrent.radyo.menemen.pro.R;
+import com.incitorrent.radyo.menemen.pro.RadyoMenemenPro;
 import com.incitorrent.radyo.menemen.pro.services.MUSIC_PLAY_SERVICE;
 
 public class NotificationControls extends BroadcastReceiver {
@@ -19,7 +22,11 @@ public class NotificationControls extends BroadcastReceiver {
             if (intent.getExtras().getBoolean("stop")) //Stop signal received from now playing notification
                 dataSource = "stop";
             else if (intent.getExtras().getString("dataSource", null) != null) //dataSource received from on air notification
+            {
+                new Menemen(context).kaydet(RadyoMenemenPro.IS_PODCAST,"hayır");
                 dataSource = intent.getExtras().getString("dataSource");
+                Toast.makeText(context, R.string.loading, Toast.LENGTH_SHORT).show();
+            }
         }
         i.putExtra("dataSource", dataSource);
         context.startService(i);
