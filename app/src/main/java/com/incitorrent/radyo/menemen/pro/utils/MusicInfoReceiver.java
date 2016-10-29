e40ab6a049efc8f6b27dabdc766f1af95d7ec97b
Lütfi Tekin <lutfi-tekin@outlook.com>
2016-10-29 21:06:42 +0300

    cleanup

--- app/src/main/java/com/incitorrent/radyo/menemen/pro/utils/MusicInfoReceiver.java
+++ app/src/main/java/com/incitorrent/radyo/menemen/pro/utils/MusicInfoReceiver.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 
 public class MusicInfoReceiver extends BroadcastReceiver {
-    private static final String TAG = "MusicInfoReceiver" ;
-    //internet bağlantısı değiştiğinde veya telefon başlatıldığında MUSIC_INFO_SERVICE i başlat
 
     public MusicInfoReceiver() {
     }
@@ -14,15 +12,7 @@ public class MusicInfoReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         m = new Menemen(context);
-//        final Boolean notify_when_onair = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("notifications_on_air", true);
-//     if(m.isInternetAvailable() && notify_when_onair) context.startService(new Intent(context, MUSIC_INFO_SERVICE.class));
-//     if(!m.isInternetAvailable() && m.oku("caliyor").equals("evet")) {
-//         //internet bağlantısı kopunca duraklat
-//         context.startService(new Intent(context, MUSIC_PLAY_SERVICE.class));
-//         Log.v(TAG,"Radyo Çalarken bağlantı gitti");
-//     }
-//        Log.v(TAG,"onReceive isInternetAvailable" + m.isInternetAvailable());
-//        if(m.isInternetAvailable()) context.startService(new Intent(context, MUSIC_INFO_SERVICE.class));
-        if(m.isConnectedWifi()) new syncChannels(context).execute(); //Güç kablosu bağlı ve wifi açık-> senkronize et
+        if(m.isConnectedWifi())
+            new syncChannels(context).execute(); //Güç kablosu bağlı ve wifi açık-> senkronize et
     }
 }
