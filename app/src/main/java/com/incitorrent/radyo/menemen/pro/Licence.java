452f24df21448700ec1a99bb34aa030599def4f1
Lütfi Tekin <lutfi-tekin@outlook.com>
2017-01-01 19:53:31 +0300

    changed chat icons

--- app/src/main/java/com/incitorrent/radyo/menemen/pro/Licence.java
+++ app/src/main/java/com/incitorrent/radyo/menemen/pro/Licence.java
@@ -32,7 +32,7 @@ public class Licence extends AppCompatActivity {
                 "notification\n" +
                 "<div>Icons made by <a href=\"http://www.flaticon.com/authors/google\" title=\"Google\">Google</a> from <a href=\"http://www.flaticon.com\" title=\"Flaticon\">www.flaticon.com</a> is licensed by <a href=\"http://creativecommons.org/licenses/by/3.0/\" title=\"Creative Commons BY 3.0\" target=\"_blank\">CC 3.0 BY</a></div>\n" +
                 "emoji\n" +
-                "<div>Icons made by <a href=\"http://www.flaticon.com/authors/yannick\" title=\"Yannick\">Yannick</a> from <a href=\"http://www.flaticon.com\" title=\"Flaticon\">www.flaticon.com</a> is licensed by <a href=\"http://creativecommons.org/licenses/by/3.0/\" title=\"Creative Commons BY 3.0\" target=\"_blank\">CC 3.0 BY</a></div>\n" +
+                "<div>Icons made by <a href=\"http://www.freepik.com\" title=\"Freepik\">Freepik</a> from <a href=\"http://www.flaticon.com\" title=\"Flaticon\">www.flaticon.com</a> is licensed by <a href=\"http://creativecommons.org/licenses/by/3.0/\" title=\"Creative Commons BY 3.0\" target=\"_blank\">CC 3.0 BY</a></div>" +
                 "default art\n" +
                 "<div>Icons made by <a href=\"http://www.freepik.com\" title=\"Freepik\">Freepik</a> from <a href=\"http://www.flaticon.com\" title=\"Flaticon\">www.flaticon.com</a> is licensed by <a href=\"http://creativecommons.org/licenses/by/3.0/\" title=\"Creative Commons BY 3.0\" target=\"_blank\">CC 3.0 BY</a></div>\n" +
                 "logout\n" +
