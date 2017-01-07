92c4ed30465e6b7b1f3d6e1098dbe03f06e92eee
Lütfi Tekin <lutfi-tekin@outlook.com>
2017-01-07 21:26:00 +0300

    private topics

--- app/src/main/java/com/incitorrent/radyo/menemen/pro/utils/topicDB.java
+++ app/src/main/java/com/incitorrent/radyo/menemen/pro/utils/topicDB.java
@@ -9,17 +9,20 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+
 /**
  * Radyo Menemen Pro Created by lutfi on 3.08.2016.
  */
 public class topicDB extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 7;
+    public static final int DATABASE_VERSION = 8;
     public static final String DATABASE_NAME = "radyomenemenproTopic.db";
     public static final String TOPICS_TABLE = "topics";
     public static final String _TOPICID = "tid";
     public static final String _TOPICSTR = "topicstr";
     public static final String _CREATOR = "creator";
     public static final String _JOINED = "joined";
+    public static final String _TYPE = "type";
     public static final String _TITLE = "title";
     public static final String _DESCR = "description";
     public static final String _IMAGEURL = "image";
@@ -42,6 +45,7 @@ public class topicDB extends SQLiteOpenHelper {
                 _TOPICSTR + " TEXT, " +
                 _CREATOR + " TEXT, " +
                 _JOINED + " INTEGER, " +
+                _TYPE + " INTEGER, " +
                 _TITLE + " TEXT, " +
                 _DESCR + " TEXT, " +
                 _IMAGEURL + " TEXT " +
@@ -70,6 +74,7 @@ public class topicDB extends SQLiteOpenHelper {
         values.put(_TOPICSTR, t.get_TOPICSTR());
         values.put(_CREATOR, t.get_CREATOR());
         values.put(_JOINED, t.get_JOINED());
+        values.put(_TYPE, t.get_PRIVATE());
         values.put(_TITLE, t.get_TITLE());
         values.put(_DESCR, t.get_DESCR());
         values.put(_IMAGEURL, t.get_IMAGEURL());
@@ -216,11 +221,19 @@ public class topicDB extends SQLiteOpenHelper {
      */
     public void join(String topicid){
         SQLiteDatabase db = getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(_JOINED,"1");
-        int rows = db.update(TOPICS_TABLE,contentValues,_TOPICID + "='" + topicid + "' ",null);
-        Log.d(TOPICS_TABLE, rows + " affected");
-        db.close();
+        try {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(_JOINED,"1");
+            int rows = db.update(TOPICS_TABLE,contentValues,_TOPICID + "='" + topicid + "' ",null);
+            Log.d(TOPICS_TABLE, rows + " affected");
+            String topicstr = getTopicSTR(topicid);
+            if(topicstr!=null)
+                FirebaseMessaging.getInstance().subscribeToTopic(topicstr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            db.close();
+        }
     }
     /**
      * Leave the topic with the given id
@@ -252,7 +265,7 @@ public class topicDB extends SQLiteOpenHelper {
      */
     public void refreshTopics(){
         SQLiteDatabase db = getWritableDatabase();
-        db.delete(TOPICS_TABLE,"1",null);
+        db.delete(TOPICS_TABLE, _TYPE + "='1'",null);
         db.close();
     }
 
@@ -309,7 +322,8 @@ public class topicDB extends SQLiteOpenHelper {
                     c.getString(c.getColumnIndex(_JOINED)),
                     c.getString(c.getColumnIndex(_TITLE)),
                     c.getString(c.getColumnIndex(_DESCR)),
-                    c.getString(c.getColumnIndex(_IMAGEURL))
+                    c.getString(c.getColumnIndex(_IMAGEURL)),
+                    c.getString(c.getColumnIndex(_TYPE))
                     );
             c.close();
             db.close();
@@ -399,9 +413,9 @@ public class topicDB extends SQLiteOpenHelper {
 
 
     public static class TOPIC {
-        public String _TOPICID,_TOPICSTR,_CREATOR,_JOINED,_TITLE,_DESCR,_IMAGEURL;
+        public String _TOPICID,_TOPICSTR,_CREATOR,_JOINED,_TITLE,_DESCR,_IMAGEURL,_PRIVATE;
 
-        public TOPIC(String _TOPICID, String _TOPICSTR, String _CREATOR, String _JOINED, String _TITLE, String _DESCR, String _IMAGEURL) {
+        public TOPIC(String _TOPICID, String _TOPICSTR, String _CREATOR, String _JOINED, String _TITLE, String _DESCR, String _IMAGEURL, String _PRIVATE) {
             this._TOPICID = _TOPICID;
             this._TOPICSTR = _TOPICSTR;
             this._CREATOR = _CREATOR;
@@ -409,62 +423,39 @@ public class topicDB extends SQLiteOpenHelper {
             this._TITLE = _TITLE;
             this._DESCR = _DESCR;
             this._IMAGEURL = _IMAGEURL;
+            this._PRIVATE = _PRIVATE;
         }
 
         public String get_TOPICID() {
             return _TOPICID;
         }
 
-        public void set_TOPICID(String _TOPICID) {
-            this._TOPICID = _TOPICID;
-        }
-
         public String get_TOPICSTR() {
             return _TOPICSTR;
-        }
-
-        public void set_TOPICSTR(String _TOPICSTR) {
-            this._TOPICSTR = _TOPICSTR;
         }
 
         public String get_CREATOR() {
             return _CREATOR;
         }
 
-        public void set_CREATOR(String _CREATOR) {
-            this._CREATOR = _CREATOR;
-        }
-
         public String get_JOINED() {
             return _JOINED;
-        }
-
-        public void set_JOINED(String _JOINED) {
-            this._JOINED = _JOINED;
         }
 
         public String get_TITLE() {
             return _TITLE;
         }
 
-        public void set_TITLE(String _TITLE) {
-            this._TITLE = _TITLE;
-        }
-
         public String get_DESCR() {
             return _DESCR;
-        }
-
-        public void set_DESCR(String _DESCR) {
-            this._DESCR = _DESCR;
         }
 
         public String get_IMAGEURL() {
             return _IMAGEURL;
         }
 
-        public void set_IMAGEURL(String _IMAGEURL) {
-            this._IMAGEURL = _IMAGEURL;
+        public String get_PRIVATE() {
+            return _PRIVATE;
         }
     }
 
